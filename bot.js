bfed3d26358c6565ec68c6144ad098648f7b0ab2
Rickard31 <vitalka.marchenko@gmail.com>
2017-12-01 13:21:33 +0200

    Swear bot

--- bot.js
+++ bot.js
@@ -7,8 +7,7 @@ var api = new telegram({
     }
 });
 
-api.on('message', function(message)
-{
+api.on('message', function (message) {
     var chat_id = message.chat.id;
 
     // It'd be good to check received message type here
@@ -19,12 +18,49 @@ api.on('message', function(message)
         chat_id: message.chat.id,
         text: message.text ? message.text : 'This message doesn\'t contain text'
     })
-        .then(function(message)
-        {
+        .then(function (message) {
             console.log(message);
+            addNewUser(message.chat);
         })
-        .catch(function(err)
-        {
+        .catch(function (err) {
             console.log(err);
         });
 });
+
+var fs = require('fs');
+
+fs.readFile('users.json', 'utf8', function (err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+        var users = JSON.parse(data).users;
+        /*users.forEach(function (t) {
+            api.sendMessage({
+                chat_id: t.id,
+                text: t.username + ' - грязный пидор'
+            })
+        })*/
+
+    }
+})
+
+function addNewUser(user) {
+    fs.readFile('users.json', function (err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            var before = JSON.parse(data);
+            var isNew = before.users.some(item => item.id === user.id);
+            //console.log('some',before.users.some(item => item.id===user.id));
+            if (!isNew) {
+                before.users.push({id: user.id, username: user.username});
+                fs.writeFile('users.json', JSON.stringify(before), function (err) {
+                    throw err;
+                })
+            }
+        }
+    })
+}
+
+
+
